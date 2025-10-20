e943ba8e0c8a596edca372c437f41ddd22e60c71
sanada04 <dasana1214@gmail.com>
2025-10-20 10:30:11 +0900

    fix: 調整

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/sanada-portfolio">
       <App />
     </BrowserRouter>
   </StrictMode>,
